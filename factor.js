bd87dbd0c4f26d4567843702d6dd1b98f7704ad2
KSHITIZNIROULA <niroulachhitiz@gmail.com>
2023-12-17 07:50:16 +0545

    updated node js

---
+++ factor.js
@@ -0,0 +1,14 @@
+
+var number = 5;
+var result = factorial(number);
+console.log("The factorial of " + number + " is: " + result);
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  }
+
+  
+  
